06d40961eb1cbdd49f5392fe227916c82f1d451c
UKhurshed <khurshedumarov777@gmail.com>
2022-08-11 18:08:29 +0500

    first commit

---
+++ megafon-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  megafon-task
+//
+//  Created by Khurshed Umarov on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
